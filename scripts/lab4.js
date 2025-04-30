11539d216e1aab252d9467e47918ac52a1380e8e
Darkcatz <andrewlichao@gmail.com>
2025-04-29 22:17:32 -0700

    ahcnge

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -14,7 +14,7 @@ function sumValues(num1, num2, add) {
         return result;
     }
     else {
-        return add;
+        return false;
     }
 }
 
@@ -27,9 +27,11 @@ function sumValues(num1, num2, add) {
 function discountPrices(prices, discount) {
     const discounted = []
     const length = prices.length;
-    let discountedPrice = 0
+    if (length == 0){
+        return false;
+    }
     for(let i = 0; i < length; i++) {
-        discountedPrice = prices[i] * (1 - discount);
+        const discountedPrice = prices[i] * (1 - discount);
         discounted.push(discountedPrice);
     }
 
